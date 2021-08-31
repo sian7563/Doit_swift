51e167fe48b2f4d86012efa1d1800d9b004cf9d5
sian7563 <sian7563@naver.com>
2021-08-31 22:08:53 +0900

    MapView

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -27,20 +27,58 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         myMap.showsUserLocation = true
     }
-    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double) {
-        let pLoaction = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double)-> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
         let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
-        let pRegion = MKCoordinateRegion(center: pLoaction, span: spanValue)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
         myMap.setRegion(pRegion, animated: true)
+        return pLocation
     }
-
+    
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue : CLLocationDegrees, delta span :Double, title strTitle: String, subtitle strSubtitle:String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let pLocation = locations.last
-        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first
+            let country = pm!.country
+            var address:String = country!
+            
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+        })
+        
+        locationManager.stopUpdatingLocation()
     
     }
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
-        
+        if sender.selectedSegmentIndex == 0 {
+            self.lblLocationInfo1.text = ""
+            self.lblLocationInfo2.text = ""
+            locationManager.startUpdatingLocation()
+            
+        } else if sender.selectedSegmentIndex == 1 {
+            setAnnotation(latitudeValue: 36.642002, longitudeValue: 127.489618, delta: 1, title: "청주시청", subtitle: "충청북도 청주시 상당구 북문로3가 상당로 155")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "청주시청"
+
+        } else if sender.selectedSegmentIndex == 2 {
+            setAnnotation(latitudeValue: 37.456546797573544, longitudeValue: 126.95004926074618, delta: 0.1, title: "서울대학교", subtitle: "서울특별시 관악구 관악로 1 서울대학교 생명과학부 504동")
+            self.lblLocationInfo1.text = "보고 계신 위치"
+            self.lblLocationInfo2.text = "서울대학교"
+        }
     }
 
 }
